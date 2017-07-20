01e4a8fe364cb79f5942c51fd740bb2ad0c86b4b
Jennifer Causing <jaycausing@Jennifers-MacBook-Pro.local>
2017-07-20 12:59:08 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function header(){
+    var head1 = document.getElementById("head1");
+    var ctx1 = head1.getContext("2d");
+    ctx1.fillStyle = "#00ffff";
+    ctx1.fillRect(0,0,head1.width,head1.height);
+}
