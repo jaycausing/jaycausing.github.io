6b1b1165ba40ad06164580c24d08feb01b44567c
Jennifer Causing <jaycausing@Jennifers-MBP.home>
2017-07-21 13:09:01 -0400

    got canvas to work

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function header(){
-    var head1 = document.getElementById("head1");
-    var ctx1 = head1.getContext("2d");
-    ctx1.fillStyle = "#00ffff";
-    ctx1.fillRect(0,0,head1.width,head1.height);
+window.onload = function(){
+    var head = document.getElementById("head1");
+    var ctx = head1.getContext("2d");
+
+    ctx.beginPath();
+    ctx.rect(0,0,head1.width,head1.height);
+    ctx.fillStyle = "#00ffff";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.rect(0,0,head1.width,head1.height);
+    var hGrad = ctx.createLinearGradient(0,0,head1.width,head1.height);
+    hGrad.addColorStop(0, 'rgba(0,0,0,0)');
+    hGrad.addColorStop(0.5, 'rgba(255, 102, 255,1)');
+    hGrad.addColorStop(1, 'rgba(0,0,0,0)');
+    ctx.fillStyle = hGrad;
+    ctx.fill();
 }
